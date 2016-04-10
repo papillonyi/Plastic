6877a62bd1a0847dabb0c3e7078071cc6b5953d3
papillonyi <papillonyi39@gmail.com>
2016-04-10 15:32:04 +1000

    beginnin

--- app/src/main/java/com/example/andy/plastic/MainActivity.java
+++ app/src/main/java/com/example/andy/plastic/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.andy.plastic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttonChatRoom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonChatRoom=(Button) findViewById(R.id.chatRoomButton);
+
+        buttonChatRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, activity_chatRoom.class));
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/andy/plastic/activity_chatRoom.java
@@ -0,0 +1,27 @@
+package com.example.andy.plastic;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Andy on 4/10/2016.
+ */
+public class activity_chatRoom extends AppCompatActivity {
+    Button ButtonChatRoomToMain;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatroom);
+        ButtonChatRoomToMain=(Button) findViewById(R.id.ButtonChatRoomToMain);
+
+        ButtonChatRoomToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(activity_chatRoom.this, MainActivity.class));
+            }
+        });
+    }
+}
